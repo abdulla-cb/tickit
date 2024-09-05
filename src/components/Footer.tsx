3b19c3f36b3fd84a534cc3cd247eba8c658525f7
Abdulla Al-Kamil <abdulla.alkamil@coinbase.com>
2024-09-05 20:59:48 +0100

    feaT: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <section className="mt-auto mb-2 flex w-full flex-col-reverse justify-between gap-2 md:mt-8 md:mb-6 md:flex-row">
       <aside className="flex items-center pt-2 md:pt-0">
         <h3 className="mr-2 mb-2 text-m md:mb-0">
-          Built with love for Base Hack Week
+          🔵 Built for Base Hack Week 🔵
         </h3>
       </aside>
     </section>
